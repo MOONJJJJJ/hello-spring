5e51697cdd336b77e729a74c76452bd186da95f7
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-04 18:06:17 +0900

    5 - 회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -27,5 +30,12 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
 
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
