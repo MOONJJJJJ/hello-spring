bbd93ee580c465090e42035851862a7d85a4237d
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-05 16:55:53 +0900

    7 - AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-@Service
+
+import org.springframework.transaction.annotation.Transactional;
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     @Autowired
